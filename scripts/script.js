102bbe4cb28a39139e7053add4f457208e94d8d4
Alex Anisimov <62463175+aadev151@users.noreply.github.com>
2021-06-12 12:39:16 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("header").innerHTML = '<div class="small_header">✊🏿 #BLM</div>';
   } else {
     document.getElementById("header").innerHTML = '<div class="blm_logo">✊🏿</div><h1>#BlackLivesMatter</h1>';

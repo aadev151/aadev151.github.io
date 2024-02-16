f7da72e085c5f7ba921b1beb5dce4baeabf948c5
Alex Anisimov <62463175+aadev151@users.noreply.github.com>
2024-02-15 22:49:14 -0500

    Update content.js - 2/15/24 - Wi-Tronix internship

--- static/js/content.js
+++ static/js/content.js
@@ -82,10 +82,12 @@ const contentByIndexes = [
     </ul>
     <p>I have prior experience with:</p>
     <ul class="skills">
+        <li>Java</li>
         <li>JavaScript</li>
         <li>jQuery</li>
         <li>c++</li>
         <li>Swift</li>
+        <li>C# and ASP.NET</li>
     </ul>
 
     <h2>Technical:</h2>
@@ -121,54 +123,58 @@ const contentByIndexes = [
         <li>Handling pressure</li>
         <li>Teamwork</li>
     </ul>
-
-    <h2>Other:</h2>
-    <ul class="skills">
-        <li>Spoken Russian</li>
-        <li>Written Russian</li>
-        <li>Russian-to-English and English-to-Russian translation</li>
-    </ul>
     `,
 
     `
     <h1>Education</h1>
     <h2>Bachelor of Science, Software Engineering, Rose-Hulman Institute of Technology, 2023-2027</h2>
     <p>
-        Related courses freshman year: Object-Oriented Software Development, Introduction to Computer
-        Systems Design, Introduction to Digital Systems, Introduction to Entrepreneurship, Practical Security I, Calculus III
+        Related courses freshman year: Data Structures and Algorithm Analysis, Object-Oriented Software Development,
+        Computer Architecture, Practical Security, Introduction to Entrepreneurship
     </p>
     <h2>Lyceum 1535, Moscow, Russia (high school, 2020-2023)</h2>
     <p>
-        General secondary education with IT concentration. Graduated with
+        General secondary education with concentration in Software Engineering. Graduated with
         <strong>5.0/5.0 GPA and 2 gold medals for outstanding academic achievements</strong>
     </p>
+
+    <h2>Google Cybersecurity Professional Certificate, Expected February 2024</h2>
 
     <h2>Online workshops from Yandex (Russian Google counterpart):</h2>
     <ul>
         <li>Python programming language basics, September 2020 - May 2021</li>
-        <li>Industrial programming on Python, September 2021 - May 2023, graduated with honors</li>
+        <li>Industrial programming in Python, September 2021 - May 2023, graduated with honors</li>
         <li>Django, October 2022 - December 2022</li>
     </ul>
     `,
 
     `
     <h1>Work experience</h1>
-    <p>
-        I worked as a freelance full-stack developer for 3 years, starting with May 2020.
-        At first, I used website builders like Tilda to create web pages, but then I started to focus more on coding
-        the pages myself.
-    </p>
 
-    <h2>The hihlights of my work as a freelancer:</h2>
+    <h2>Software Intern, Wi-Tronix, Bolingbrook, IL, Summer 2024</h2>
+
+    <h2>Software Engineering Intern, Rose-Hulman Ventures, Terre Haute, IN, Dec 2023 - Present</h2>
     <ul>
-        <li>Develop personal web pages and websites for businesses, such as a jewelry store, a pharmacy, and a confectionery</li>
-        <li>Build both front end and back end, utilizing HTML, CSS, and Python and advise customers on the design of web pages</li>
-        <li>Volunteer as a web developer for nonprofits</li>
+        <li>Building a production website with ASP.NET, C#, and SignalR using Visual Studio and Microsoft SQL Server Management Studio</li>
+        <li>Collaborate with a team of 35 people to efficiently bring the solution to the customer</li>
+    </ul>
+
+    <h2>
+        I also currently work part-time as a receptionist at the Rose-Hulman Sports & Recreation Center.
+    </h2>
+
+    <h2>Freelance Full-Stack Web Developer, March 2020 - June 2023</h2>
+    <ul>
+        <li>Developed landing pages, personal web pages, business card sites, and corporate websites. Created 10+ solutions</li>
+        <li>Used HTML, CSS, and JavaScript along with third-party modules, such as intl-tel-input.js, bulma.css, and others, for the front end</li>
+        <li>Utilized Python, namely Flask, Flask-Login, and Flask-SQLAlchemy, for the back end. Worked with databases, using pure SQL and ORMs. Developed admin panels</li>
+        <li>Frequently advised clients on design and content of their web pages. Worked closely with customers to bring their ideas to life. Dealt well with ambiguity and undefined or broadly defined problems</li>
+        <li>Volunteered as a web developer for nonprofits, such as charities aiding children with disabilities and elderly people, making sure they can get a website for free</li>
     </ul>
 
     <h2>Featured work:</h2>
     <p>
-        Some of my recent works include these web pages:
+        Some of my most recent works include these web pages:
     </p>
     <div class="link" onclick="redirect('https://nogotochki.xyz')">
         <h1>Nail salon web page</h1>
@@ -185,6 +191,20 @@ const contentByIndexes = [
     `,
 
     `
+    <h1>Research</h1>
+    <p>
+        I am currently (<strong>Sep 2023 - Present</strong>) involved in undergraduate Computer Science Education research with Dr. Joseph Hollingsworth. Highlights:
+    </p>
+    <ul>
+        <li>Research Automating Methods to Overcome Learning Obstacles in Computer Science</li>
+        <li>Develop software analyzing students’ assignment-taking patterns using Python</li>
+        <li>Track time spent on problem, length of breaks, and tendency to guess and check</li>
+        <li>Investigate the best ways to interpret results and visualize data</li>
+        <li>Optimize and refactor code from past researchers</li>
+    
+    `,
+
+    `
     <h1>My featured projects</h1>
     
     <h2>HelpUnicorn</h2>
@@ -195,9 +215,11 @@ const contentByIndexes = [
     </i>
     <ul>
         <li>Founded a network of welfare funds and IT specialists to offer free assistance to charities</li>
-        <li>Implemented website’s authentication (including email confirmation and password reset), database, private chat, and API; co-authored iOS app; developed mini website builder for charities</li>
-        <li>Motivated teammates, represented the site at conferences, enabled collaboration with foreign funds and developers</li>
-        <li><strong>Saved $3000+ for charities and encouraged 1000+ users to join</strong></li>
+        <li>Wrote 2,000 lines of code for authentication, email confirmation, database, private chat, password reset, and API; contributed to templates and CSS rules; co-authored iOS app</li>
+        <li>Developed website builder for charities to automate the process of webpage creation</li>
+        <li>Motivated teammates, organized weekly progress meetings, represented site at conferences, enabled collaboration with foreign funds and developers</li>
+        <li>Optimized website performance to get 99% according to Google PageSpeed Insights</li>
+        <li><strong>Saved $3,000+ for charities and encouraged 1,000+ users to join</strong></li>
     </ul>
     <div class="link" onclick="redirect('https://helpunicorn.pythonanywhere.com')">
         <h1>HelpUnicorn</h1>
@@ -211,9 +233,8 @@ const contentByIndexes = [
         Mar 2023 - May 2023
     </i>
     <ul>
-        <li>Built a website identifying ChatGPT-written text in 50+ languages</li>
-        <li>Employed OpenAI API; used JavaScript to get text from an input field or uploaded files and support multi-thread upload</li>
-        <li>Currently discussing integration into the admissions process for Rose-Hulman Institute of Technology</li>
+        <li>Built an app (web-client and server) identifying ChatGPT-written text in 50+ languages</li>
+        <li>Used JavaScript and Python to implement multi-tiered application (two-tier architecture on top of OpenAI API) sending queries and fetching responses from the server</li>
     </ul>
 
     <h2>Kitty Sneakers</h2>
@@ -224,13 +245,14 @@ const contentByIndexes = [
     </i>
     <ul>
         <li>Started a mock sneaker store page to provide all functionality an online store expects</li>
-        <li>Used Django and Bootstrap to work with database, fixtures, admin page, authorization, posting and deleting products, star rating, product galleries, tags</li>
-        <li>Utilized Git, worked with branches and Github issues and pull requests; employed environment variables, .env file, and .gitignore        </li>
-        <li>Learned working with virtual environments</li>
+        <li>Used Django and Bootstrap to work with relational database, fixtures, admin page, authorization, star rating, tags, and managing products</li>
+        <li>Worked with CI/CD, Git branches, and GitHub issues and pull requests; employed environment variables, .env file, and .gitignore</li>
+        <li>Learned to work with virtual environments</li>
+        <li>Tested each chunk of code with regular and edge cases and reviewed code of classmates</li>
     </ul>
-    <div class="link" onclick="redirect('https://gitbub.com/aadev151/kitty-sneakers')">
+    <div class="link" onclick="redirect('https://gihtub.com/aadev151/kitty-sneakers')">
         <h1>Kitty Sneakers</h1>
-        <p>https://gitbub.com/<wbr>aadev151/<wbr>kitty-sneakers</p>
+        <p>https://github.com/<wbr>aadev151/<wbr>kitty-sneakers</p>
     </div>
 
     <h2>Duckystyle</h2>
@@ -243,9 +265,9 @@ const contentByIndexes = [
         <li>Designed a system to buy and sell pictures of ducks, top up balance, withdraw money, and get currency for ads to mock an NFT auction and educate users about NFTs</li>
         <li>Coordinated team and arranged weekly meetings to ensure project success</li>
     </ul>
-    <div class="link" onclick="redirect('https://gitbub.com/aadev151/duckystyle')">
+    <div class="link" onclick="redirect('https://gihtub.com/aadev151/duckystyle')">
         <h1>Duckystyle</h1>
-        <p>https://gitbub.com/<wbr>aadev151/<wbr>duckystyle</p>
+        <p>https://gihtub.com/<wbr>aadev151/<wbr>duckystyle</p>
     </div>
 
     <h2>Squid Pygame</h2>
@@ -259,9 +281,9 @@ const contentByIndexes = [
         <li>Designed the concept of the levels to captivate users’ attention and repeat the series’ plot</li>
         <li>Led team of two, organizing regular meetings and making sure deadlines were hit</li>
     </ul>
-    <div class="link" onclick="redirect('https://gitbub.com/aadev151/squid-pygame')">
+    <div class="link" onclick="redirect('https://gihtub.com/aadev151/squid-pygame')">
         <h1>Squid Pygame</h1>
-        <p>https://gitbub.com/<wbr>aadev151/<wbr>squid-pygame</p>
+        <p>https://gihtub.com/<wbr>aadev151/<wbr>squid-pygame</p>
     </div>
 
     <h2>MP3 Player</h2>
@@ -275,9 +297,9 @@ const contentByIndexes = [
         <li>Developed an app capable of loading tracks, reading their metadata (including name, artist, and cover image), creating playlists, and greeting users with the name they indicate upon registration</li>
         <li>Worked with Git to control versions of the project</li>
     </ul>
-    <div class="link" onclick="redirect('https://gitbub.com/aadev151/mp3-player')">
+    <div class="link" onclick="redirect('https://gihtub.com/aadev151/mp3-player')">
         <h1>MP3 Player</h1>
-        <p>https://gitbub.com/<wbr>aadev151/<wbr>mp3-player</p>
+        <p>https://gihtub.com/<wbr>aadev151/<wbr>mp3-player</p>
     </div>
 
     <h2>prevlink.js</h2>
@@ -357,6 +379,26 @@ const contentByIndexes = [
 
     `
     <h1>Leadership</h1>
+    <h2>Rose-Hulman</h2>
+    <ul>
+        <li>
+        <p><strong>Open Source Club, Vice President</strong></p>
+            <p>
+                I am the co-founder of Rose-Hulman Open Source Club, which is a place where students can
+                interested in coding can discuss their projects, find teammates, or just hang out with
+                like-minded folks. We also provide funding for software engineering side projects.
+                <i>Starting Spring 2024</i>
+            </p>
+        </li>
+        <li>
+            <p><strong>Coordinator of Noblitt Scholars Social Committee</strong></p>
+            <p>
+                I organize meeetings, reserve rooms, and make minutes for weekly meetings of the committee.
+                I am responsible for contacting the organizations the Committee decided to cooperate with for
+                social events for Noblitt Scholars.
+            </p>
+        </li>
+    </ul>
     <h2>Lyceum 1535</h2>
     <ul>
         <li>
@@ -370,7 +412,7 @@ const contentByIndexes = [
         <li>
             <p><strong>Head of Publications at Student Newspaper</strong></p>
             <p>
-                Overall, we issued 15+ papers. I contributed byy editing errors in publications, choosing
+                Overall, we issued 15+ papers. I contributed by editing errors in publications, choosing
                 and discussing topics for new posts, and approving pictures.
             </p>
         </li>
@@ -390,10 +432,27 @@ const contentByIndexes = [
     <h2>Rose-Hulman Institute of Technology</h2>
     <ul>
         <li>
+            <p><strong>Member of the R^2 program</strong></p>
+            <p>
+                Rose Squared is a combined Bachelor of Science and Master of Engineering Management program
+                enabling highly motivated students to obtain both degrees in just four years. R^2 tudents take
+                as many or more credits as those who decide to obtain the Master's degree in two years.
+            </p>
+        </li>
+        <li>
+            <p><strong>Noblitt Scholars STEM Outreach Program</strong></p>
+            <p><i>Web Developer, Software Developer, Computer Science Mentor</i></p>
+            <p>
+                As part of the STEM Outreach Program, I participate in activities organized for kids in Terre Haute
+                and meant for them to familiarize themselves with STEM disciplines. Most recently, we held a series
+                of workshops for Brownies and Juniors Girl Scouts. Our next event is for Seniors and Ambassadors Girl Scouts
+            </p>
+        </li>
+        <li>
             <p><strong>Computer Security Club</strong></p>
             <p>
-                Computer Security Club at Rose-Hulman educates its members about the cybersecurity field in CompSci
-                through activities, guest speakers, conferences, and competitions.
+                We meet weekly to discuss different topics related to cybersecurity. We also participate in team competitions,
+                both red teaming and blue teaming, and have weekly CTF challenges.
             </p>
         </li>
         <li>
@@ -404,13 +463,6 @@ const contentByIndexes = [
                 and developing approaches to capture value in the marketplace.
             </p>
         </li>
-        <li>
-            <p><strong>Member of the Rose Squared program</strong></p>
-            <p>
-                Rose Squared is a combined Bachelor of Science and Master of Engineering Management program
-                enabling highly motivated students to obtain both degrees in just four years.
-            </p>
-        </li>
     </ul>
     `,
 
@@ -418,13 +470,14 @@ const contentByIndexes = [
     <h1>Contact me</h1>
     <p>
         If you want to reach out to me, be that about my software engineering experience or funny panda videos, feel free
-        to send me an email. My address is <a href="mailto:anisima@rose-hulman.edu">anisima@rose-hulman.edu</a>
+        to send me an email. My email address is <a href="mailto:anisima@rose-hulman.edu">anisima@rose-hulman.edu</a>
     </p>
     <p>
         Here are some other ways to reach out to me:
     </p>
     <ul>
-        <li><a href="https://wa.me/79032736345" target="_blank">WhatsApp</a></li>
+        <li><a href="tel:+18125433024" target="_blank">812-543-3024</a></li>
+        <li><a href="https://wa.me/18125433024" target="_blank">WhatsApp</a></li>
         <li><a href="https://linkedin.com/in/aleksandr-anisimov" target="_blank">LinkedIn</a></li>
         <li><a href="https://snapchat.com/add/aadev151" target="_blank">Snapchat</a></li>
     </ul>
